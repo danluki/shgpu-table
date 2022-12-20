21c68bc6b3826a4b4ce4a74d618f5fae25905a13
danilluk1 <danluki02@yandex.ru>
2022-12-20 12:34:44 +0000

    dashboard

--- apps/frontend/pages/_app.tsx
+++ apps/frontend/pages/_app.tsx
@@ -1,8 +1,88 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import {
+  AppShell,
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+  useMantineTheme,
+} from "@mantine/core";
+import { useColorScheme, useHotkeys, useLocalStorage } from "@mantine/hooks";
+import { ModalsProvider } from "@mantine/modals";
+import { NotificationsProvider } from "@mantine/notifications";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { useState } from "react";
+import { SWRConfig } from "swr";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App(props: AppProps) {
+  const { Component } = props;
+
+  const preferredColorScheme = useColorScheme();
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: "theme",
+    defaultValue: preferredColorScheme,
+    getInitialValueInEffect: true,
+  });
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
+  useHotkeys([["mod+J", () => toggleColorScheme()]]);
+
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
+
+  return (
+    <>
+      <Head>
+        <title>Расписание ШГПУ</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
+      >
+        <MantineProvider
+          theme={{ colorScheme }}
+          withGlobalStyles
+          withNormalizeCSS
+        >
+          <NotificationsProvider>
+            <SWRConfig
+              value={{
+                onError: (error, key) => {
+                  console.log("here");
+                  console.log(error, key);
+                },
+                // fetcher: swrAuthFetcher,
+              }}
+            >
+              <ModalsProvider>
+                <AppShell
+                  styles={{
+                    main: {
+                      background:
+                        colorScheme === "dark"
+                          ? theme.colors.dark[8]
+                          : theme.colors.gray[0],
+                    },
+                  }}
+                  navbarOffsetBreakpoint="sm"
+                  asideOffsetBreakpoint="sm"
+                  navbar={
+                    <SideBar
+                      opened={opened}
+                      header={<NavBar setOpened={setOpened} opened={opened} />}
+                    />
+                  }
+                ></AppShell>
+              </ModalsProvider>
+            </SWRConfig>
+          </NotificationsProvider>
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </>
+  );
 }
-
-export default MyApp
