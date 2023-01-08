b4e1904476f11a14c907bc090787bd99c2c4360b
danilluk1 <danluki02@yandex.ru>
2023-01-08 18:28:49 +0000

    notify message parser + unit tests

--- apps/tg-bot/internal/parser/parser.go
+++ apps/tg-bot/internal/parser/parser.go
@@ -1,16 +1,77 @@
 package parser
 
-import "time"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"time"
 
-type ProcessedMessage struct {
-	FacultyId uint8     `json:"facultyId"`
-	Link      string    `json:"link"`
-	WeekBegin time.Time `json:"weekBegin"`
-	WeekEnd   time.Time `json:"weekEnd"`
+	"github.com/go-playground/validator/v10"
+)
+
+type ParsedMessage struct {
+	FacultyId uint8     `json:"facultyId" validate:"required"`
+	Link      string    `json:"link"      validate:"required"`
+	WeekBegin time.Time `json:"weekBegin" validate:"required"`
+	WeekEnd   time.Time `json:"weekEnd"   validate:"required"`
 	IsNew     bool      `json:"isNew"`
 	IsUpdated bool      `json:"isUpdated"`
 }
 
-func ProcessMessage(msg string) (string, error) {
+type ResultMessage struct {
+	Faculty uint8
+	Message string
+}
 
+func ParseMessage(msg string, curTime time.Time) (*ResultMessage, error) {
+	var pm ParsedMessage
+	err := json.Unmarshal([]byte(msg), &pm)
+	if err != nil {
+		return nil, err
+	}
+	//TODO: Validator must be global object to enalble caching
+	err = validator.New().Struct(pm)
+	if err != nil {
+		return nil, err
+	}
+	if err != nil {
+		return nil, err
+	}
+	if (pm.IsNew && pm.IsUpdated) || (!pm.IsNew && !pm.IsUpdated) {
+		return nil, errors.New("table must be created OR updated")
+	}
+
+	rm := &ResultMessage{
+		Faculty: 0,
+		Message: "",
+	}
+	if pm.IsNew {
+		if curTime.After(pm.WeekBegin) && curTime.Before(pm.WeekEnd) {
+			rm.Message = fmt.Sprintf(
+				"Появилось расписание на текущую неделю, вы можете получить расписание по ссылке %s или введя команду: пары на неделю",
+				pm.Link,
+			)
+		} else if curTime.Before(pm.WeekBegin) {
+			rm.Message = fmt.Sprintf(
+				"Появилось расписание на следущую неделю, вы можете получить расписание по ссылке %s или введя команду: пары на след неделю",
+				pm.Link,
+			)
+		}
+	}
+
+	if pm.IsUpdated {
+		if curTime.After(pm.WeekBegin) && curTime.Before(pm.WeekEnd) {
+			rm.Message = fmt.Sprintf(
+				"Обновилось расписание на текущую неделю, вы можете получить расписание по ссылке %s или введя команду: пары на неделю",
+				pm.Link,
+			)
+		} else if curTime.Before(pm.WeekBegin) {
+			rm.Message = fmt.Sprintf(
+				"Обновилось расписание на следущую неделю, вы можете получить расписание по ссылке %s или введя команду: пары на след неделю",
+				pm.Link,
+			)
+		}
+	}
+
+	return rm, nil
 }
--- apps/tg-bot/internal/parser/parser_test.go
+++ apps/tg-bot/internal/parser/parser_test.go
@@ -4,63 +4,120 @@ import (
 	"strings"
 	"testing"
 	"time"
+
+	"github.com/danilluk1/shgpu-table/apps/tg-bot/internal/di"
+	"github.com/go-playground/validator"
+	"github.com/samber/do"
 )
 
 var testTime time.Time
 
 func TestMain(m *testing.M) {
-	testTime, err := time.Parse("2006-01-02", "2022-12-27")
+	validator := validator.New()
+	do.ProvideValue(di.Provider, *validator)
+
+	tt, err := time.Parse("2006-01-02", "2022-12-27")
 	if err != nil {
 		panic("Invalid data string passed")
 	}
+	testTime = tt
 
 	m.Run()
 }
 
 func Test_ParseMessage_BadFormat(t *testing.T) {
-	msg := "{\"facultId\":11,\"isNe\":true,\"isUpdated\":true,\"weekBegin\":\"2022-12-26T00:00:00.000Z\",\"weekEnd\":\"2023-01-01T00:00:00.000Z\",\"link\":\"https://shgpi.edu.ru/fileadmin/rasp/faculty/f11/26_12_2022_01_01_2023/26_12_2022_01_01_2023.xls'}\""
-	respMsg, err := ParseMessage(msg)
+	msg := `{"facultId":11,"isNew":true,"isUpdated":true,"weekBegin":"2022-12-26T00:00:00.000Z","weekEnd":"2023-01-01T00:00:00.000Z","link":"https://shgpi.edu.ru/fileadmin/rasp/faculty/f11/26_12_2022_01_01_2023/26_12_2022_01_01_2023.xls"}`
+	_, err := ParseMessage(msg, testTime)
 	if err == nil {
-		t.Error("incorrect result: expected error for invalid message")
+		t.Fatalf("incorrect result: %v", err)
+	}
+}
+
+func Test_ParseMessage_ValidFormat(t *testing.T) {
+	msg := `{"facultyId":11,"isNew":false,"isUpdated":true,"weekBegin":"2022-12-26T00:00:00.000Z","weekEnd":"2023-01-01T00:00:00.000Z","link":"https://shgpi.edu.ru/fileadmin/rasp/faculty/f11/26_12_2022_01_01_2023/26_12_2022_01_01_2023.xls"}`
+	_, err := ParseMessage(msg, testTime)
+	if err != nil {
+		t.Fatalf("incorrect result: %v", err)
 	}
 }
 
 func Test_ParseMessage_NewlyCreated(t *testing.T) {
-	msg := "{\"facultyId\":11,\"isNew\":true,\"isUpdated\":true,\"weekBegin\":\"2022-12-26T00:00:00.000Z\",\"weekEnd\":\"2023-01-01T00:00:00.000Z\",\"link\":\"https://shgpi.edu.ru/fileadmin/rasp/faculty/f11/26_12_2022_01_01_2023/26_12_2022_01_01_2023.xls'}\""
-	respMsg, err := ParseMessage(msg)
+	msg := `{"facultyId":11,"isNew":true,"isUpdated":false,"weekBegin":"2022-12-26T00:00:00.000Z","weekEnd":"2023-01-01T00:00:00.000Z","link":"https://shgpi.edu.ru/fileadmin/rasp/faculty/f11/26_12_2022_01_01_2023/26_12_2022_01_01_2023.xls"}`
+	resMsg, err := ParseMessage(msg, testTime)
 	if err != nil {
-		t.Error("incorrect result: expected newly created response")
+		t.Fatalf("incorrect result: %v", err)
 	}
-
-	if !strings.Contains(respMsg, "текущую") {
+	t.Log(resMsg)
+	if !strings.Contains(resMsg.Message, "текущую") {
 		t.Error("incorrect result: response must be for current week")
 	}
 
-	if !strings.Contains(respMsg, "новое") {
+	if !strings.Contains(resMsg.Message, "Появилось") {
 		t.Error("incorrect result: response must contains info about new table")
 	}
 }
 
 func Test_ParseMessage_Updated(t *testing.T) {
-	msg := "{\"facultyId\":11,\"isNew\":false,\"isUpdated\":true,\"weekBegin\":\"2022-12-26T00:00:00.000Z\",\"weekEnd\":\"2023-01-01T00:00:00.000Z\",\"link\":\"https://shgpi.edu.ru/fileadmin/rasp/faculty/f11/26_12_2022_01_01_2023/26_12_2022_01_01_2023.xls'}\""
-	respMsg, err := ParseMessage(msg)
+	msg := `{"facultyId":11,"isNew":false,"isUpdated":true,"weekBegin":"2022-12-26T00:00:00.000Z","weekEnd":"2023-01-01T00:00:00.000Z","link":"https://shgpi.edu.ru/fileadmin/rasp/faculty/f11/26_12_2022_01_01_2023/26_12_2022_01_01_2023.xls"}`
+	resMsg, err := ParseMessage(msg, testTime)
 	if err != nil {
-		t.Error("incorrect result: expected updated table response")
+		t.Fatalf("incorrect result: %v", err)
 	}
-
-	if !strings.Contains(respMsg, "текущую") {
+	if !strings.Contains(resMsg.Message, "текущую") {
 		t.Error("incorrect result: response must be for current week")
 	}
 
-	if !strings.Contains(respMsg, "обновилось") {
+	if !strings.Contains(resMsg.Message, "Обновилось") {
 		t.Error("incorrect result: response must contains info about new table")
 	}
 }
 
-func Test_ParseMessage_NewlyCreated_NextWeek(t *testing.T) {
+func Test_ParseMessage_UpdatedAndCreated(t *testing.T) {
+	msg := `{"facultyId":11,"isNew":true,"isUpdated":true,"weekBegin":"2022-12-26T00:00:00.000Z","weekEnd":"2023-01-01T00:00:00.000Z","link":"https://shgpi.edu.ru/fileadmin/rasp/faculty/f11/26_12_2022_01_01_2023/26_12_2022_01_01_2023.xls"}`
+	_, err := ParseMessage(msg, testTime)
+	if err == nil {
+		t.Fatalf("incorrect result: must be error")
+	}
+}
 
+func Test_ParseMessage_NewlyCreated_NextWeek(t *testing.T) {
+	msg := `{"facultyId":11,"isNew":true,"isUpdated":false,"weekBegin":"2023-01-01T00:00:00.000Z","weekEnd":"2023-01-08T00:00:00.000Z","link":"https://shgpi.edu.ru/fileadmin/rasp/faculty/f11/26_12_2022_01_01_2023/26_12_2022_01_01_2023.xls"}`
+	resMsg, err := ParseMessage(msg, testTime)
+	if err != nil {
+		t.Fatalf("incorrect result: %v", err)
+	}
+	if !strings.Contains(resMsg.Message, "следущую") {
+		t.Error("incorrect result: response must be for next week")
+	}
+
+	if !strings.Contains(resMsg.Message, "Появилось") {
+		t.Error("incorrect result: response must contains info about new table")
+	}
 }
 
 func Test_ParseMessage_Updated_NextWeek(t *testing.T) {
+	msg := `{"facultyId":11,"isNew":false,"isUpdated":true,"weekBegin":"2023-01-01T00:00:00.000Z","weekEnd":"2023-01-08T00:00:00.000Z","link":"https://shgpi.edu.ru/fileadmin/rasp/faculty/f11/26_12_2022_01_01_2023/26_12_2022_01_01_2023.xls"}`
+	resMsg, err := ParseMessage(msg, testTime)
+	if err != nil {
+		t.Fatalf("incorrect result: %v", err)
+	}
+	if !strings.Contains(resMsg.Message, "следущую") {
+		t.Error("incorrect result: response must be for current week")
+	}
 
+	if !strings.Contains(resMsg.Message, "Обновилось") {
+		t.Error("incorrect result: response must contains info about new table")
+	}
+}
+
+func Test_ParseMessage_PastWeek(t *testing.T) {
+	msg := `{"facultyId":11,"isNew":false,"isUpdated":true,"weekBegin":"2022-12-20T00:00:00.000Z","weekEnd":"2022-12-26T00:00:00.000Z","link":"https://shgpi.edu.ru/fileadmin/rasp/faculty/f11/26_12_2022_01_01_2023/26_12_2022_01_01_2023.xls"}`
+	resMsg, err := ParseMessage(msg, testTime)
+	if err != nil {
+		t.Fatalf("incorrect result: %v", err)
+	}
+	t.Log(resMsg)
+	if resMsg.Message != "" {
+		t.Fatal("Must be empty message")
+	}
 }
